7b32507526bc5c54d8864516f17c78fc80ff0191
pshenichniyinfo <33541550+pshenichniyinfo@users.noreply.github.com>
2023-10-24 08:57:24 +0300

    feat: added menu for admin ww

--- src/resources/views/components/menu.blade.php
+++ src/resources/views/components/menu.blade.php
@@ -1,30 +1,32 @@
-@foreach($menus as $menu)
-    <li class="nav-item">
-        @isset($menu['child'])
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-{{ $menu['icon'] }}"></i>
-                <p>
-                    {{ $menu['name'] }}
-                    <i class="fas fa-angle-left right"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                @foreach($menu['child'] as $child)
-                    <li class="nav-item">
-                        <a href="{{ $child['link'] }}" class="nav-link">
-                            {{ $menu['name'] }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        @else
-            <a href="{{ $menu['link'] }}" class="nav-link">
-                <i class="nav-icon fas fa-{{ $menu['icon'] }}"></i>
-                <p>
-                    {{ $menu['name'] }}
-                </p>
-            </a>
-        @endempty
+@isset($menus)
+    @foreach($menus as $menu)
+        <li class="nav-item">
+            @isset($menu['child'])
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-{{ $menu['icon'] }}"></i>
+                    <p>
+                        {{ $menu['name'] }}
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    @foreach($menu['child'] as $child)
+                        <li class="nav-item">
+                            <a href="{{ $child['link'] }}" class="nav-link">
+                                {{ $menu['name'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <a href="{{ $menu['link'] }}" class="nav-link">
+                    <i class="nav-icon fas fa-{{ $menu['icon'] }}"></i>
+                    <p>
+                        {{ $menu['name'] }}
+                    </p>
+                </a>
+            @endempty
 
-    </li>
-@endforeach
+        </li>
+    @endforeach
+@endisset
